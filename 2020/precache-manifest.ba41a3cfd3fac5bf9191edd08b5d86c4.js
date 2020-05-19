097bef0acd71f8193bb052e885c5890745c5a10a
Jobin Jacob Kavalam <jjkavalam@gmail.com>
2020-05-19 15:15:05 +0800

    add 2020

---
+++ 2020/precache-manifest.ba41a3cfd3fac5bf9191edd08b5d86c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66af9baa7fee1bff600e3e7152ed2367",
+    "url": "/2020/index.html"
+  },
+  {
+    "revision": "153b6001bfc1cf352794",
+    "url": "/2020/static/css/main.09c936aa.chunk.css"
+  },
+  {
+    "revision": "a52e60d1df86155500c8",
+    "url": "/2020/static/js/2.78b3621d.chunk.js"
+  },
+  {
+    "revision": "153b6001bfc1cf352794",
+    "url": "/2020/static/js/main.e54a2579.chunk.js"
+  },
+  {
+    "revision": "fb7ba275cdbdded2dbd6",
+    "url": "/2020/static/js/runtime~main.8ee6e4f6.js"
+  }
+]);
